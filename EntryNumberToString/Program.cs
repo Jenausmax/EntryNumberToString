881899ae24d7e80a6292347b09f58db7c18bfd55
Jenausmax <mminmax@gmail.com>
2021-04-29 15:44:09 +0300

    Add project files.

---
+++ EntryNumberToString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntryNumberToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
